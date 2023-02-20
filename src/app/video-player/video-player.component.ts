3960e01ec2fc24b199f217d26a3f94dbf3bd8b37
Roman Rudi <roman.rudi@outlook.com>
2023-02-20 10:54:25 +0200

    Add video tag to test retrieving video from server

---
+++ src/app/video-player/video-player.component.ts
@@ -0,0 +1,27 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.css']
+})
+export class VideoPlayerComponent implements OnInit {
+  @ViewChild('videoPlayer') videoPlayerRef: ElementRef;
+
+  videoUrl: string = "";
+
+  constructor(elem: ElementRef) {
+    this.videoPlayerRef = elem;
+  }
+
+  ngOnInit() {
+    const videoPath = 'http://localhost:8080/media/video/1.mp4';
+    this.videoUrl = this.getHlsUrl(videoPath);
+  }
+
+  getHlsUrl(url: string): string {
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    const hlsUrl = isSafari ? url.replace('.mp4', '.m3u8') : url;
+    return hlsUrl;
+  }
+}
