c573758acc06ade1951bab301a14a30c36fa5533
Roman Rudi <roman.rudi@outlook.com>
2023-03-07 12:23:13 +0200

    Some styles for welcome page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LandingComponent} from "./landing/landing.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'app', component: AppComponent, canActivate: [AuthorizeGuard] },
+  { path: '', component: LandingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
